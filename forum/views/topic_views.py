936113fbf0529069823b3b1b71865835c29afc87
IsisAriele <isisariele1@gmail.com>
2023-10-12 23:59:27 -0300

    Criação de tópicos (#11)

---
+++ forum/views/topic_views.py
@@ -0,0 +1,43 @@
+from django.views import View
+from django.shortcuts import render, redirect
+from forum.models import Topic
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from forum.forms.topic_form import TopicRegistrationForm
+
+
+class TopicListView(View):
+    @method_decorator(login_required)
+    def get(self, request):
+        # O query param da requisição serve para realizar o filtro.
+        # Query param nesse caso é o "?my_topics=true" informado na URL.
+        query_topic_by_user = request.GET.get("my_topics", False)
+
+        if not query_topic_by_user:
+            topics = Topic.objects.all().order_by("-created_at")
+        else:
+            topics = Topic.objects.filter(user=request.user).order_by("-created_at")
+
+        context = {"topics": topics}
+        return render(request, "forum/index.html", context)
+
+
+class TopicRegistrationView(View):
+    def get(self, request):
+        context = {"form": TopicRegistrationForm()}
+        return render(request, "forum/topic.html", context)
+    
+    def post(self, request):
+        form = TopicRegistrationForm(request.POST)
+        if form.is_valid():
+            topic = Topic(
+                title=form.data["title"],
+                text=form.data["text"],
+                user=request.user,
+            )
+            topic.save()
+
+            return redirect("index")
+        
+        # Deu ruim
+        return render(request, "forum/topic.html", {})
