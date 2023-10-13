eebe5a9e82dd3944311dd2f0c3254c4687f0ce0b
IsisAriele <isisariele1@gmail.com>
2023-10-12 23:37:19 -0300

    Criação de tópicos (#11)

--- forum/urls.py
+++ forum/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from forum.views.user_views import UserRegisterView, UserLoginView
-from forum.views.topic_views import TopicListView
+from forum.views.topic_views import TopicListView, TopicRegistrationView
 
 urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"), 
     path("login/", UserLoginView.as_view(), name="login"),
     path("index/", TopicListView.as_view(), name="index"),
+    path("topic/", TopicRegistrationView.as_view(), name="topic"),
 ]
